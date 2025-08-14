759e9b1730c0b0b7018e5557dfbe089f09b6ea7d
Prabhakar Singh <157056538+prabhakarsin909@users.noreply.github.com>
2025-08-15 02:02:17 +0530

    Add files via upload

---
+++ Report/fv/fifo1/read_libs.tcl
@@ -0,0 +1,6 @@
+add_search_path ../../../../../../../vlsi/pdk/tsmc_gp_65_stdio/tcbn65gplus_200a/TSMCHOME/digital/Front_End/timing_power_noise/CCS/tcbn65gplus_200a/ -library -both
+read_library -liberty -both \
+    /home/users/prabhakar/work_tsmc_gp_65/self_project/rough/report/../../../../../../../vlsi/pdk/tsmc_gp_65_stdio/tcbn65gplus_200a/TSMCHOME/digital/Front_End/timing_power_noise/CCS/tcbn65gplus_200a/tcbn65gpluswc_ccs.lib \
+    /home/users/prabhakar/work_tsmc_gp_65/self_project/rough/report/../../../../../../../vlsi/pdk/tsmc_gp_65_stdio/tcbn65gplus_200a/TSMCHOME/digital/Front_End/timing_power_noise/CCS/tcbn65gplus_200a/tcbn65gplusbc_ccs.lib \
+    /home/users/prabhakar/work_tsmc_gp_65/self_project/rough/report/../../../../../../../vlsi/pdk/tsmc_gp_65_stdio/tcbn65gplus_200a/TSMCHOME/digital/Front_End/timing_power_noise/CCS/tcbn65gplus_200a/tcbn65gplustc_ccs.lib
+
